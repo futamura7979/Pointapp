af2cad37d5e3580cf7548c3b6119e4951cf656e6
Ryo futamura <futamura.kunnren@gmail.com>
2025-07-16 00:29:22 +0900

    no message

---
+++ myproject/myapp/App.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+from selenium.common.exceptions import NoSuchElementException
+
+def run_selenium_script():
+
+    # Chromeドライバーを起動
+    driver = webdriver.Chrome()
+
+    # ログインページにアクセス
+    driver.get("https://pointi.jp/entrance.php")
+
+    time.sleep(1)
+
+    # name=passとname=pass の属性をとって入力 
+    email_input = driver.find_element(By.NAME, "email_address")
+    email_input.send_keys("futamura.kunnren@gmail.com")
+
+    pass_input = driver.find_element(By.NAME, "password")
+    pass_input.send_keys("kikikiki")
+
+    #クリック
+    link = driver.find_element(By.NAME, "Submit")
+    link.click()
+
+    time.sleep(1)
+
+    close_button = driver.find_element(By.ID, "cboxClose")
+    close_button.click()
+
+
+    element = driver.find_element(By.XPATH, '//span[text()="毎日クリック"]')
+    element.click()
+
+
+    elements = driver.find_elements(By.CSS_SELECTOR, ".go_btn")
+    text = "クリックで1pt"
+
+
+
+    for i, element in enumerate(elements):
+        if element.text ==  text:
+            print(f"{i}番目: {element.text}")
+            element.click()
+            time.sleep(2)
+            
+            
+            
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .App import run_selenium_script
 
-def index(req):
-    return HttpResponse('Hello World')
+
+def pointget(request):
+    run_selenium_script()
+    return HttpResponse("Seleniumスクリプトを実行しました。")
+
+def index(request):
+    return render(request, 'myapp/sample.html')
+
